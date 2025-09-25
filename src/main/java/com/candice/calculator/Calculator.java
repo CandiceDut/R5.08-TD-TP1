c3a8033ae1202ab4514f6964d2d4e7afa72eec3a
Candice Dutournier <cdutourni001@iutbayonne.univ-pau.fr>
2025-09-25 11:56:40 +0200

    exceptions add, divide (exo 11)

--- src/main/java/com/candice/calculator/Calculator.java
+++ src/main/java/com/candice/calculator/Calculator.java
@@ -1,11 +1,40 @@
 package com.candice.calculator;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class Calculator {
     public static int add(int opG, int opD) {
+        if(opG + opD >= Integer.MAX_VALUE){
+            throw new IllegalArgumentException("L'addition dépasse les limites max du int.");
+        }
+        else if(opG+opD <= Integer.MIN_VALUE){
+            throw new IllegalArgumentException("L'addition dépasse les limites min du int.");
+        }
         return opG + opD;
     }
 
     public static int divide(int opG, int opD) {
+        if(opD == 0){
+            throw new IllegalArgumentException("Division par 0 impossible.");
+        }
         return opG / opD;
+    }
+
+    public static Set<Integer> ensembleChiffres(int pNombre){
+        Set<Integer> chiffres = new HashSet<>();
+        int n = Math.abs(pNombre);
+
+        if (n == 0) {
+            chiffres.add(0);
+        }
+
+        while (n > 0) {
+            int chiffre = n % 10;
+            chiffres.add(chiffre);
+            n /= 10;
+        }
+
+        return chiffres;
     }
 }
--- src/test/java/com/candice/calculator/CalculatorTest.java
+++ src/test/java/com/candice/calculator/CalculatorTest.java
@@ -7,28 +7,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static com.candice.calculator.Calculator.add;
-import static com.candice.calculator.Calculator.divide;
+import java.util.Set;
+
+import static com.candice.calculator.Calculator.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
-
-    @ParameterizedTest
-    @CsvSource({
-            "0, 1, 1",
-            "1, 2, 3",
-            "-2, 2, 0",
-            "0, 0, 0",
-            "-1, -2, -3"
-    })
-
-    void add_parametre(int nb1, int nb2, int nb3){
-        //WHEN
-        int result = add(nb1, nb2);
-
-        //THEN
-        assertThat(result).isEqualTo(nb3);
-    }
 
     @Test
     void add_calcule_la_somme_de_deux_int(){
@@ -54,5 +39,85 @@ public class CalculatorTest {
 
         //THEN
         assertThat(result).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3"
+    })
+
+    void add_parametre(int nb1, int nb2, int nb3){
+        //WHEN
+        int result = add(nb1, nb2);
+
+        //THEN
+        assertThat(result).isEqualTo(nb3);
+    }
+
+    @Test
+    void ensemble_chiffre_renvoi_les_chiffres_uniques(){
+        //GIVEN
+        int nb = 1231234;
+
+        //WHEN
+        Set<Integer> result = ensembleChiffres(nb);
+
+        //THEN
+        assertThat(result).isEqualTo(Set.of(1, 2, 3, 4));
+    }
+
+    @Test
+    void divide_diviseur_egal_a_0(){
+        //GIVEN
+        int apG = 5;
+        int apD = 0;
+
+        //WHEN
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            divide(apG, apD);
+        });
+
+        //THEN
+        assertThat(exception)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Division par 0 impossible.");
+    }
+
+    @Test
+    void add_erreur_quand_depasse_max_value(){
+        //GIVEN
+        int apG = Integer.MAX_VALUE;
+        int apD = 0;
+
+        //WHEN
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            add(apG, apD);
+        });
+
+        //THEN
+        assertThat(exception)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("L'addition dépasse les limites max du int.");
+    }
+
+    @Test
+    void add_erreur_quand_depasse_min_value(){
+        //GIVEN
+        int apG = Integer.MIN_VALUE;
+        int apD = 0;
+
+        //WHEN
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            add(apG, apD);
+        });
+
+        //THEN
+        assertThat(exception)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("L'addition dépasse les limites min du int.");
     }
 }
