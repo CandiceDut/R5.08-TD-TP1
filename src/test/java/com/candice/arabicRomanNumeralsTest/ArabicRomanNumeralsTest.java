627f19f874ba2d59a9bc39e3eb42e2bcad0729c0
Candice Dutournier <cdutourni001@iutbayonne.univ-pau.fr>
2025-09-29 10:59:48 +0200

    exo3 cycle 3 additions ok soustractions et max erreur

--- src/test/java/com/candice/arabicRomanNumeralsTest/ArabicRomanNumeralsTest.java
+++ src/test/java/com/candice/arabicRomanNumeralsTest/ArabicRomanNumeralsTest.java
@@ -40,4 +40,68 @@ public class ArabicRomanNumeralsTest {
         //THEN
         assertThat(result).isEqualTo("I");
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, II",
+            "3, III",
+            "5, V",
+            "10, X",
+            "20, XX",
+            "50, L",
+            "100, C",
+            "500, D",
+            "700, DCC",
+            "1000, M",
+            "2016, MMXVI",
+            "2017, MMXVII",
+            "2018, MMXVIII",
+            "2020, MMXX",
+            "2021, MMXXI",
+            "2022, MMXXII",
+            "2023, MMXXIII",
+            "2025, MMXXV"
+    })
+    void convert_chiffres_simples_fait_la_bonne_addition(int chiffre, String romain){
+        //WHEN
+        String result = chiffreRomain.convert(chiffre);
+
+        //THEN
+        assertThat(result).isEqualTo(romain);
+    }
+
+    @Test
+    void convert_max(){
+        //GIVEN
+        int chiffre = 3999;
+
+        //WHEN
+        String result = chiffreRomain.convert(chiffre);
+
+        //THEN
+        assertThat(result).isEqualTo("MMMCMXCIX");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, IV",
+            "9, IX",
+            "39, XXXIX",
+            "499, ID",
+            "1970, MCMLXX",
+            "1971, MCMLXXI",
+            "1972, MCMLXXII",
+            "1973, MCMLXXIII",
+            "1974, MCMLXXIV",
+            "1975, MCMLXXV",
+            "2019, MMXIX",
+            "2024, MMXXIV"
+    })
+    void convert_chiffres_soustraits_fait_la_bonne_soustraction(int chiffre, String romain){
+        //WHEN
+        String result = chiffreRomain.convert(chiffre);
+
+        //THEN
+        assertThat(result).isEqualTo(romain);
+    }
 }
